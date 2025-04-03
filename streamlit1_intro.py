63ce1a9a78cf3a83ab7d5de795b9d1e6c4346511
Paola <paolacastillo0407@ciencias.unam.mx>
2025-04-03 16:02:45 -0600

    tablas f

--- streamlit1_intro.py
+++ streamlit1_intro.py
@@ -196,11 +196,10 @@ st.pyplot(fig6)
 #inciso e) ------------------------------------------------------------------------
 
 st.subheader("Eficiencia de estimaciones")
-st.dataframe(CR.df_final.style.background_gradient(cmap='Blues'),hide_index = True)
+st.dataframe(CR.df_final.style.background_gradient(cmap='Blues',subset=['Porcentaje Violaciones (%)']),hide_index = True)
 
 #inciso f) ------------------------------------------------------------------------------------------
 
 CR.tabla_violaciones
 
-st.dataframe(CR.tabla_violaciones.style.background_gradient(cmap="Blues",subset=['Porcentaje Violaciones (%)']),hide_index = True)
 st.dataframe(CR.tabla_violaciones.style.applymap(MCF.highlight_high_values, subset=['Porcentaje Violaciones (%)']),hide_index = True)
